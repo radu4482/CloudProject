5d4d38739c69809763673e8b1fe3e7c90962bf64
Besleaga Radu-George <62284900+radu4482@users.noreply.github.com>
2021-06-03 01:07:05 +0300

    Add files via upload

--- Project/main.py
+++ Project/main.py
@@ -1,12 +1,14 @@
 import random
-from google.cloud import storage
-from flask import Flask, request, make_response, Response
-from google.cloud.storage import Blob
-import os
 import mysql.connector
 from mysql.connector.constants import ClientFlag
+from google.cloud import storage
+from flask import Flask, request, make_response, Response
+from flask_cors import CORS, cross_origin
+from google.cloud.storage import Blob
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 storage_client = storage.Client()
 
@@ -119,8 +121,8 @@ def utilizatorByName(username):
     return resp
 
 
-@app.route('/utilizatori/<user_id>/visits/<marker_id>', methods=["POST"])
-def visits(user_id, marker_id):
+@app.route('/utilizatori/<username>/visits/<oras>/<marker_name>', methods=["POST"])
+def visits(username,oras, marker_name):
     if request.method not in ["POST"]:
         return method_not_allowed(request.method, request.path, ["POST"])
 
@@ -128,8 +130,9 @@ def visits(user_id, marker_id):
     cursor = cnxn.cursor()
     resp = Response("")
     if request.method == "POST":
-        if verifyUserExists(user_id) and  verifyMonumentExists(marker_id):
-            cursor.execute("Insert INTO Vizited (user_id,marker_id) Values(%s,%s)" % (user_id, marker_id))
+        #if verifyUserExists(user_name) and  verifyMonumentExists(marker_id):
+        cursor.execute("Insert INTO Vizited Select Utilizator.user_id,Markers.marker_id FROM Markers "
+                       "INNER JOIN Utilizator WHERE Markers.Oras='%s' and Markers.Titlu='%s' and Utilizator.username='%s'"% (oras,marker_name,username))
         out = cursor.fetchall()
         cnxn.commit()
         cnxn.close()
@@ -149,8 +152,8 @@ def visits(user_id, marker_id):
         resp.headers["Content-Type"] = "application/json"
 
 
-@app.route('/utilizatori/<user_id>/visits', methods=["GET"])
-def visited(user_id):
+@app.route('/utilizatori/<user_name>/visits/<oras>', methods=["GET"])
+def visited(user_name,oras):
     if request.method not in ["GET"]:
         return method_not_allowed(request.method, request.path, ["GET"])
 
@@ -158,11 +161,14 @@ def visited(user_id):
     cursor = cnxn.cursor()
     resp = Response("")
     if request.method == "GET":
-        cursor.execute("Select * from Vizited where user_id=%s" % user_id)
+        cursor.execute("Select Markers.marker_id, Markers.Titlu, Markers.Oras, Markers.Latitudine, Markers.Longitudine, Markers.URL  from Markers "
+                       "INNER JOIN Vizited ON Markers.marker_id=Vizited.marker_id "
+                       "JOIN Utilizator ON Utilizator.user_id=Vizited.user_id and Utilizator.username='%s' "
+                       "WHERE Markers.Oras='%s'" % (user_name,oras))
         out = cursor.fetchall()
         cnxn.close()
         myResponse = {
-            bucket[0]: {"user_id": bucket[0], "monument_id": bucket[1]}
+            bucket[0]:{"Titlu": bucket[1], "Oras": bucket[2],"Latitudine": bucket[3],"Longitudine": bucket[4],"URL": bucket[5]}
             for bucket in out}
 
     resp = make_response(myResponse)
@@ -288,7 +294,8 @@ def cityMonument(nume_oras, monument_title):
             resp.headers["Content-Type"] = "application/json"
 
     if request.method == "POST":
-        if verifyDuplicat(nume_oras, monument_title) == True:
+        request_json = request.json
+        if verifyDuplicat(nume_oras, monument_title)== True :
             myResponse = {
                 "Response": "This monument is allready registered in this city"
             }
@@ -323,234 +330,6 @@ def cityMonument(nume_oras, monument_title):
         resp.status_code = 200  # not found
         resp.headers["Content-Type"] = "application/json"
     return resp
-
-
-@app.route('/buckets', methods=["GET", "POST"])
-def buckets():
-    if request.method not in ["GET", "POST"]:
-        return method_not_allowed(request.method, request.path, ["GET", "POST"])
-
-    resp = Response("")
-
-    if request.method == "GET":
-        buckets = storage_client.list_buckets()
-        buckets = list(filter(lambda x: "id" in x.labels.keys(), buckets))
-
-        if buckets is []:
-            buckets_dicts = {}
-        else:
-            buckets_dicts = {
-                bucket.labels['id']: {"data": {"name": bucket.name, "storageClass": bucket.storage_class}, "Actions": {
-                    "DeteleResource": {"URL": f"/buckets/{bucket.labels['id']}", "Method": "DELETE"}}} for bucket in
-                buckets}
-
-        resp = make_response(buckets_dicts)
-        resp.status_code = 200
-        resp.headers["Content-Type"] = "application/json"
-
-    elif request.method == "POST":
-
-        request_json = request.json
-
-        if "name" not in request_json.keys() or "storageClass" not in request_json.keys():
-            return bad_request(request.method, request.path)
-
-        bucket_id = random.randint(1, int(2 ** 64))
-        buckets = storage_client.list_buckets()
-
-        if "id" in [label for label in [bkt.labels for bkt in buckets]]:
-            bucket_ids = [i.labels["id"] for i in buckets]
-        else:
-            bucket_ids = []
-
-        while bucket_id in bucket_ids:
-            bucket_id = random.randint(1, int(2 ** 64))
-
-        bucket = storage_client.bucket(request_json["name"])
-        bucket.storage_class = request_json["storageClass"]
-
-        bucket.labels = {"id": str(bucket_id)}
-
-        print(bucket.labels)
-        new_bucket = storage_client.create_bucket(bucket, location="europe-west3")
-
-        response_dict = {"id": bucket.labels["id"], "Actions": {
-            "DeleteResource": {"URL": f"/buckets/{bucket.labels['id']}", "Method": "DELETE"}
-            , "ReplaceResource": {"URL": f"/buckets/{bucket.labels['id']}", "Method": "PUT"}
-            , "GetResource": {"URL": f"/buckets/{bucket.labels['id']}", "Method": "GET"}}}
-
-        myResponse = {
-
-        }
-        print(response_dict)
-        resp = make_response(response_dict)
-
-        resp.status_code = 200
-        resp.headers["Content-Type"] = "application/json"
-    return resp
-
-
-@app.route('/buckets/<id>', methods=["GET", "PUT", "POST", "DELETE"])
-def bucket(id):
-    if request.method not in ["GET", "PUT", "POST", "DELETE"]:
-        return method_not_allowed(request.method, request.path, ["GET", "PUT", "POST", "DELETE"])
-
-    if request.method == "GET":
-
-        bucket = getBucket(id)
-        if bucket == []:
-            bucket_dicts = {}
-        else:
-            buckets_dicts = {
-                bucket.labels['id']: {"data": {"name": bucket.name, "storageClass": bucket.storage_class}, "Actions": {
-                    "DeteleResource": {"URL": f"/buckets/{bucket.labels['id']}", "Method": "DELETE"}}}
-            }
-        resp = make_response(buckets_dicts)
-        resp.status_code = 200
-        resp.headers["Content-Type"] = "application/json"
-
-    # pun pe id ul specificat
-    if request.method == "PUT":
-        request_json = request.json
-        if "name" not in request_json.keys() or "storageClass" not in request_json.keys():
-            return bad_request(request.method, request.path)
-
-        deleteBucket(id)
-        bucket = postBucket(request_json["name"], request_json["storageClass"], id)
-        bucket_dicts = getBucketInfo(bucket)
-        resp = make_response(bucket_dicts)
-
-    # ignore
-    if request.method == "POST":
-        pass
-
-    if request.method == "DELETE":
-        resp = deleteBucket(id)
-        pass
-
-    return resp
-
-
-@app.route("/buckets/<int:bct_id>/objects", methods=["GET", "POST"])
-def objects(bct_id):
-    if request.method not in ["GET", "POST"]:
-        return method_not_allowed(request.method, request.path, ["GET", "POST"])
-
-    response = {}
-
-    if request.method == "GET":
-
-        buckets = storage_client.list_buckets()
-        bucket = next(filter(lambda x: "id" in x.labels.keys() and int(x.labels["id"]) == bct_id, buckets), -1)
-
-        if bucket == -1:
-            return not_found(f"{bct_id}", request.path)
-
-        blobs = storage_client.list_blobs(bucket.name)
-        blobs = list(filter(lambda x: x.metadata is not None and "id" in x.metadata.keys(), blobs))
-
-        if blobs:
-            blobs_dict = {blob.metadata["id"]: {"data": {"name": blob.name, "mime-type": blob.content_type,
-                                                         "content": blob.download_as_bytes().decode()}, "Actions": {}}
-                          for blob in blobs}
-        else:
-            blobs_dict = {}
-
-        response = make_response(blobs_dict)
-        response.status_code = 200
-        response.headers["Content-Type"] = "application/json"
-
-    elif request.method == "POST":
-
-        if "data" not in request.json.keys() or "name" not in request.json["data"] or "content" not in request.json[
-            "data"] or "mime-type" not in request.json["data"]:
-            return bad_request(request.method, request.path)
-
-        buckets = storage_client.list_buckets()
-        bucket = next(filter(lambda x: "id" in x.labels.keys() and int(x.labels["id"]) == bct_id, buckets), -1)
-
-        if bucket == -1:
-            return not_found(f"{bct_id}", request.path)
-
-        blob_id = random.randint(1, int(2 ** 64))
-        blobs = storage_client.list_blobs(bucket.name)
-        blob_ids = []
-        if "id" in [meta for meta in [blob.metadata for blob in blobs]]:
-            blob_ids = [i.metadata["id"] for i in blobs]
-
-        while blob_id in blob_ids:
-            blob_id = random.randint(1, int(2 ** 64))
-
-        blob_object = bucket.blob(request.json["data"]["name"])
-        blob_object.metadata = {"id": blob_id}
-
-        # print(f"{request.json['data']['content']}  {type(request.json['data']['content'])}")
-
-        blob_object.upload_from_string(request.json["data"]["content"], content_type=request.json["data"]["mime-type"])
-
-        response_dict = {"id": blob_object.metadata["id"], "Actions": {
-            "DeleteResource": {"URL": f"/buckets/{blob_object.metadata['id']}", "Method": "DELETE"}
-            , "ReplaceResource": {"URL": f"/buckets/{blob_object.metadata['id']}", "Method": "PUT"}
-            , "GetResource": {"URL": f"/buckets/{blob_object.metadata['id']}", "Method": "GET"}}}
-
-        print(response_dict)
-        response = make_response(response_dict)
-        response.status_code = 201
-        response.headers["Content-Type"] = "application/json"
-
-    return response
-
-
-@app.route('/buckets/<int:bkt_id>/objects/<int:obj_id>', methods=["GET", "PUT", "DELETE"])
-def object(bkt_id, obj_id):
-    if request.method not in ["GET", "PUT", "DELETE"]:
-        return method_not_allowed(request.method, request.path, ["GET", "PUT", "DELETE"])
-
-    response = {}
-
-    if request.method == "GET":
-
-        buckets = storage_client.list_buckets()
-        bucket = next(filter(lambda x: "id" in x.labels.keys() and int(x.labels["id"]) == bkt_id, buckets), -1)
-        # bucket = next(filter(lambda x: "id" in x.labels.keys() and int(x.labels["id"]) == bct_id, buckets), -1)
-        if bucket == -1:
-            return not_found(f"{bkt_id}", request.path)
-
-        blobs = storage_client.list_blobs(bucket.name)
-        blob = next(
-            filter(lambda x: x.metadata is not None and "id" in x.metadata.keys() and int(x.metadata["id"]) == obj_id,
-                   blobs), -1)
-
-        if blob != -1:
-            blobs_dict = {blob.metadata["id"]: {"data": {"name": blob.name, "mime-type": blob.content_type,
-                                                         "content": blob.download_as_bytes().decode()}, "Actions": {}}}
-        else:
-            blobs_dict = {}
-
-        response = make_response(blobs_dict)
-        response.status_code = 200
-        response.headers["Content-Type"] = "application/json"
-
-    elif request.method == "DELETE":
-
-        buckets = storage_client.list_buckets()
-        bucket = next(filter(lambda x: "id" in x.labels.keys() and int(x.labels["id"]) == bkt_id, buckets), -1)
-        # bucket = next(filter(lambda x: "id" in x.labels.keys() and int(x.labels["id"]) == bct_id, buckets), -1)
-        if bucket == -1:
-            return not_found(f"{bkt_id}", request.path)
-
-        blobs = storage_client.list_blobs(bucket.name)
-        blob = next(
-            filter(lambda x: x.metadata is not None and "id" in x.metadata.keys() and int(x.metadata["id"]) == obj_id,
-                   blobs), -1)
-
-        if blob != -1:
-            blob.delete()
-        else:
-            return not_found(f"Object {obj_id}", request.path)
-
-    return response
-
 
 def insertIntoSQL(Titlu, Oras, Descriere, Latitudine, Longitudine, URL):
     cnxn = mysql.connector.connect(**config)
@@ -595,7 +374,7 @@ def deleteMonument(Oras, Titlu):
 
 
 # Same as inserIntoSQL
-def addMonument(Oras, Titlu, Descriere, Latitudine, Longitudine, ):
+def addMonument(Oras, Titlu, Descriere, Latitudine, Longitudine, URL):
     cnxn = mysql.connector.connect(**config)
     cursor = cnxn.cursor()
     cursor.execute(
